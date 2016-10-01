720007b6e78a65729a874de89705764a7dbbd71b
aleksanderlaba <aleksanderlaba@gmail.com>
2016-10-01 13:03:34 +0200

    Add project files.

---
+++ ChangeInputLanguage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsInput;
+
+namespace ChangeInputLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cmd -> cd -> "ChangeInputLanguage.exe 10 15"
+
+            Thread.Sleep(int.Parse(args[0]));
+
+            InputSimulator.SimulateKeyDown(VirtualKeyCode.LWIN);
+            InputSimulator.SimulateKeyPress(VirtualKeyCode.SPACE);
+            InputSimulator.SimulateKeyUp(VirtualKeyCode.LWIN);
+
+            Thread.Sleep(int.Parse(args[1]));
+        }
+    }
+}
